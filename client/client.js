dccfd006e8bd5da3e1e172439e55046a7645e523
Andrew Straw <strawman@astraw.com>
2014-09-05 01:15:50 +0200

    rendering of tables seems to work

--- client/client.js
+++ client/client.js
@@ -1,11 +1,67 @@
+function get_driver_lines( my_obj ) {
+  var result = [];
+  if ('driver_lines' in my_obj) {
+    result = my_obj.driver_lines.map( function (_id) { return DriverLines.findOne(_id) } );
+   }
+  return result;
+}
+
+function get_neuron_types( my_obj ) {
+  var result = [];
+  if ('neuron_types' in my_obj) {
+    result = my_obj.neuron_types.map( function (_id) { return NeuronTypes.findOne(_id) } );
+   }
+  return result;
+}
+
+function get_neuropiles( my_obj ) {
+  var result = [];
+  if ('neuropiles' in my_obj) {
+    result = my_obj.neuropiles.map( function (_id) { return Neuropiles.findOne(_id) } );
+   }
+  return result;
+}
+
+// -------------
+
 Template.driver_lines.driver_line_cursor = function () {
   return DriverLines.find({});
 }
+
+Template.driver_line_show.neuron_types = function () {
+  return get_neuron_types(this);
+}
+
+Template.driver_line_show.neuropiles = function () {
+  return get_neuropiles(this);
+}
+
+// -------------
 
 Template.neuron_types.neuron_type_cursor = function () {
   return NeuronTypes.find({});
 }
 
+Template.neuron_type_show.driver_lines = function () {
+  return get_driver_lines(this);
+}
+
+Template.neuron_type_show.neuropiles = function () {
+  return get_neuropiles(this);
+}
+
+// -------------
+
 Template.neuropiles.neuropile_cursor = function () {
   return Neuropiles.find({});
 }
+
+Template.neuropile_show.driver_lines = function () {
+  return get_driver_lines(this);
+}
+
+Template.neuropile_show.neuron_types = function () {
+  return get_neuron_types(this);
+}
+
+// -------------
