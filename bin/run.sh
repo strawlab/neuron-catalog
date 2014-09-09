2b84760156a11144e530e0fb585d4c57f9ac278a
Andrew Straw <strawman@astraw.com>
2014-09-09 18:12:38 +0200

    persist docker state in known directory

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -d \
     --name fly-neuron-catalog \
     -p 3000:80 \
-    -v /fly-neuron-catalog/.meteor/local \
+    -v ${THISDIR}/../.docker-meteor-local:/fly-neuron-catalog/.meteor/local \
     strawlab/fly-neuron-catalog
