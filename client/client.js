18e6e4663466bb76f42a1ce93a7b09d602ca5812
Andrew Straw <strawman@astraw.com>
2014-09-14 13:02:41 +0200

    do not use font-awesome meteor package

--- client/client.js
+++ client/client.js
@@ -1,3 +1,19 @@
+// -------------
+// font awesome (see
+// https://github.com/nate-strauser/meteor-font-awesome/blob/master/load.js )
+
+var head = document.getElementsByTagName('head')[0];
+
+//Generate a style tag
+var style = document.createElement('link');
+style.type = 'text/css';
+style.rel = "stylesheet";
+style.href = '//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css';
+
+head.appendChild(style);
+
+// -------------
+
 Meteor.subscribe('driver_lines');
 Meteor.subscribe('neuron_types');
 Meteor.subscribe('neuropils');
