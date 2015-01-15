55baf0a68bb5b8251f5baceb7577fd3a276d1304
Andrew Straw <strawman@astraw.com>
2015-01-15 14:41:10 +0100

    docker: attempt to run bundle with appropriate settings

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -d \
     --name neuron-catalog \
     --link neuron-catalog-db:db \
+    --env "METEOR_SETTINGS=$(cat ${THISDIR}/../server/server-config.json)" \
     -p 3000:80 \
     strawlab/neuron-catalog
