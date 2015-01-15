d2222eda8f52f1c45aaaa3f5c9fbb2a5c944efc4
Andrew Straw <strawman@astraw.com>
2015-01-15 16:17:11 +0100

    docker: meteor settings from env for tail-mongo.py

--- server/tools/binary-upload-processor.sh
+++ server/tools/binary-upload-processor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -d \
     --link neuron-catalog-db:db \
     --name neuron-catalog-upload-processor \
+    --env "METEOR_SETTINGS=$(cat ${THISDIR}/../server-config.json)" \
     strawlab/neuron-catalog-binary-upload-processor
