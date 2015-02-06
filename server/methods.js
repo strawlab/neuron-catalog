c18e514a92eaed3355974e4e0368fe20eb049e5a
Andrew Straw <strawman@astraw.com>
2015-02-06 07:37:43 +0100

    rename files

---
+++ server/methods.js
@@ -0,0 +1,17 @@
+Meteor.methods({
+  verify_AWS_configuration: function () {
+    var failures = get_slingshot_AWS_failures();
+    return failures;
+  },
+  get_specializations: function () {
+    var x = Meteor.settings.NeuronCatalogSpecializations;
+    if (typeof x !== "undefined" && x !== null) {
+      return x;
+    } else {
+      return [];
+    }
+  },
+  remove_from_s3: function(doc) {
+    console.log("FIXME: delete from S3:", doc.s3_region, doc.s3_bucket, doc.s3_key );
+  }
+});
