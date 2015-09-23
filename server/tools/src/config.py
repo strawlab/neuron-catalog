33d104b2584f34ade2762c841638e9b5586cf8fb
Andrew Straw <strawman@astraw.com>
2015-09-23 11:45:48 +0200

    remove outdated server/tools directory

--- server/tools/src/config.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-from singleton_config import run_config_program
-
-def main():
-    run_config_program(desc="get or set neuron catalog configuration",
-                       collection_name="neuron_catalog_config")
-    
-if __name__=='__main__':
-    main()
